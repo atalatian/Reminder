fe06b608bb5cffa929f0ed6fddc874acca67a40f
atalatian <amirhoseyntalatyan@gmail.com>
2021-02-17 19:32:11 +0330

    break

--- toDo/models.py
+++ toDo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+    def __str__(self):
+        return self.category
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ManyToManyField(Category, default="uncategorized")
+    priority = models.IntegerField()
+    schedule = models.DateTimeField()
+    def __str__(self):
+        return self.title
